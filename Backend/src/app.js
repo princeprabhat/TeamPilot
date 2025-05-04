fbd553949962c0228f2770b808ea096c7e765cef
princeprabhat <princeprabhat1996@gmail.com>
2025-05-04 13:32:28 +0530

    Error handler middlewares done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,31 @@
 import express from "express";
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import { errorHandler } from "./middlewares/error";
+import routes from './routes/v1';
+import ApiError from "./utils/ApiError";
+import httpStatus from 'http-status'
 
 const app = express();
 
 
-app.get('/local', (req, res) => {
-    res.send('Hello World! I am Prince');
+app.use(helmet());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(compression())
+
+app.use(cors())
+app.options('*', cors())
+
+
+app.use('/v1', routes)
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+    next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
+app.use(errorHandler)
 
 export default app;
---
+++ Backend/src/middlewares/error.js
@@ -0,0 +1,30 @@
+import httpStatus from 'http-status';
+import ApiError from "../utils/ApiError";
+import config from "../config/config";
+
+
+
+
+const errorHandler = (err, req, res, next) => {
+    let { statusCode, message } = err;
+
+    if (!(err instanceof ApiError)) {
+        statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+        message = 'Internal Server Error';
+    }
+    res.locals.errorMessage = message;
+
+    const response = {
+        code: statusCode,
+        message,
+        ...(config.env === 'development' && { stack: err.stack })
+    };
+
+    if (config.env === 'development') {
+        console.error(err)
+    }
+
+    res.status(statusCode).send(response);
+};
+
+export { errorHandler }
